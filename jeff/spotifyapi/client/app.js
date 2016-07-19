e07d9631064c3deb2213c0acf997ae19cf129cab
Jeff <babybluejeff@gmail.com>
2016-07-19 01:34:07 -0700

    Implemented player to take Spotify, YouTube and Soundcloud links

--- jeff/spotifyapi/client/app.js
+++ jeff/spotifyapi/client/app.js
@@ -1,24 +1,29 @@
-var requestAuth = () => {
-  $.ajax
-};
+$(document).ready(() => {
+	$("#music-form").on('submit', e => {
+		// Prevents page from refreshing upon form submission
+		e.preventDefault();
 
-var searchSpotify = () => {
-  $('#search-spotify').submit(function() {
-    var query = $('.search').val();
-    $('.search').val('');
+		// Grabs the URL posted by the user
+		var link = $('.link').val();
 
-    $.ajax({
-      url: 'https://api.spotify.com/v1/search',
-      data: {
-        q: query,
-        type: 'album,artist,playlist,track'
-      },
-      success: function(response) {
-        console.log('Successfully retrieved data!', response);
-      },
-      error: function(response) {
-        console.log('Error retrieving data...');
-      }
-    });
-  });
-};
+		if (link.indexOf('spotify') !== -1) {
+			// Embed Spotify widget
+			$('#widget-container').html(`<iframe class="widget" src="https://embed.spotify.com/?uri=${link}" width="300" height="380" frameborder="0" allowtransparency="true"></iframe>`);
+		} else if (link.indexOf('youtube') !== -1) {
+			// Embed YouTube video
+			link = link.slice(32, link.length);
+			$('#widget-container').html(`<iframe width="300" height="168" src="https://www.youtube.com/embed/${link}?autoplay=1" frameborder="0" allowfullscreen></iframe>`);
+		} else if (link.indexOf('soundcloud') !== -1) {
+	    // Embed Soundcloud widget
+	    SC.initialize({ client_id: '3c90ccc4687932be44fa9433df91f18c' });
+
+	    SC.oEmbed(link, { auto_play: true }).then(oEmbed => {
+	    	var chopped = oEmbed.html.slice(66, oEmbed.html.length - 10);
+	    	$('#widget-container').html(`<iframe width="300" height="200" scrolling="no" frameborder="no" ${chopped}></iframe>`);
+	    });
+		}
+
+		// Clears the input form
+		$('.link').val('');
+	});
+});
